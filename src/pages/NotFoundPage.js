e14bb20c441a4504087a2be0536c6848dc6ce0aa
who0803 <10rlatkdtn@naver.com>
2023-07-13 08:49:30 +0900

    Style: Style NotFoundPage

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,6 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
 function NotFoundPage() {
   return (
-    <div>NotFound</div>
+    <NotFoundContainer>NotFound</NotFoundContainer>
   )
 }
 
